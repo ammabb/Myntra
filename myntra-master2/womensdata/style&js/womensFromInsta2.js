107182d72b8013e2a59c7b214e41d0c986361d44
radha231 <143729741+radha231@users.noreply.github.com>
2024-07-15 18:54:06 +0530

    Add files via upload

---
+++ myntra-master2/womensdata/style&js/womensFromInsta2.js
@@ -0,0 +1,323 @@
+var womensData = [{
+    image_url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRX4gNJP0Ec6boiEGm_NmO3RHvfA4BmeGjaCg&usqp=CAU",
+    brand: "Marks & Spencer",
+    para: "Women UV lens Sunglasses ",
+    rs: 791,
+    price: "Rs. 791",
+    strikedoffprice: "Rs. 3298",
+    offer: "(76% OFF)",
+    wishList: "WISHLIST ♡",
+    addToBag: "ADD TO BAG",
+},
+{
+    image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQC26kifCPh_fIIUqUfvLG7Is4j-EbvAA3i3A&usqp=CAU",
+    brand: "WROGN",
+    para: "Women Aviator sunglasses",
+    rs: 758,
+    price: "Rs. 758",
+    strikedoffprice: "Rs. 3299",
+    offer: "(65% OFF)",
+    wishList: "WISHLIST ♡",
+    addToBag: "ADD TO BAG",
+},
+{
+    image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRN2RoFWOpnRvFinFNumEznF_eY_HlabEkFpQ&usqp=CAU",
+    brand: "WROGN",
+    para: "Women Cateye sunglasses",
+    rs: 749,
+    price: "Rs. 749",
+    strikedoffprice: "Rs. 1299",
+    offer: "(65% OFF)",
+    wishList: "WISHLIST  ♡",
+    addToBag: "ADD TO BAG",
+},
+{
+    image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdw6ft0OYxujJlsXp_GCrzq393i2-fvuViKA&usqp=CAU",
+    brand: "Ted Smith",
+    para: "Women UV protection lens Sunglasses",
+    rs: 1199,
+    price: "Rs. 1199",
+    strikedoffprice: "Rs. 2599",
+    offer: "(65% OFF)",
+    wishList: "WISHLIST ♡",
+    addToBag: "ADD TO BAG",
+},
+{
+    image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQC1MLYZkUYMzmT6WV3OPaN2oBbK5HZnI-ayg&usqp=CAU",
+    brand: "Marks & Spencer",
+    para: "Women UV lens Sunglasses",
+    rs: 879,
+    price: "Rs. 879",
+    strikedoffprice: "Rs. 2499",
+    offer: "(65% OFF)",
+    wishList: "WISHLIST ♡",
+    addToBag: "ADD TO BAG",
+},
+{
+    image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSEtXCPb9vGIL98OH-YHwaf9KjNcPkwyKmCQ&usqp=CAU",
+    brand: "WROGN",
+    para: "Women Wayfarer sunglasses",
+    rs: 674,
+    price: "Rs. 674",
+    strikedoffprice: "Rs. 1499",
+    offer: "(65% OFF)",
+    wishList: "WISHLIST ♡",
+    addToBag: "ADD TO BAG",
+},
+{
+    image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScbJAEzrgNc_AEzTMhI95R8oo42ODUZosAhg&usqp=CAU",
+    brand: "Ted Smith",
+    para: "Women Wayfarer sunglasses",
+    rs: 1035,
+    price: "Rs. 1035",
+    strikedoffprice: "Rs. 2799",
+    offer: "(63% OFF)",
+    wishList: "WISHLIST ♡",
+    addToBag: "ADD TO BAG",
+},
+{
+    image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTg--V_FHE9yKXUhdIP1b85fghmWo17ffXFo6bc2PGny_uDYRo31MCJxE_HNDo0774I5_k&usqp=CAU",
+    brand: "Ted Smith",
+    para: "Women UV lens Sunglasses",
+    rs: 1479,
+    price: "Rs. 1479",
+    strikedoffprice: "Rs. 3699",
+    offer: "(60% OFF)",
+    wishList: "WISHLIST ♡",
+    addToBag: "ADD TO BAG",
+},
+{
+    image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7AdCg3kM-K1lqjc6DTTatp-3Bqonu2JW1W5941l8RBEF3aHKjoqX7epJnxhV5TpvncEI&usqp=CAU",
+    brand: "WROGN",
+    para: "Women Aviator sunglasses",
+    rs: 805,
+    price: "Rs. 805",
+    strikedoffprice: "Rs. 1299",
+    offer: "(65% OFF)",
+    wishList: "WISHLIST ♡",
+    addToBag: "ADD TO BAG",
+},
+{
+    image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQAUMYvGukYkRxbKM7-0H1XbehY0ix3Fb5hA&usqp=CAU",
+    brand: "Women Cateye sunglassesh",
+    para: "Women Charming Pink Shirt for Casual wear",
+    rs: 489,
+    price: "Rs. 489",
+    strikedoffprice: "Rs. 999",
+    offer: "(65% OFF)",
+    wishList: "WISHLIST ♡",
+    addToBag: "ADD TO BAG",
+},
+{
+    image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVGMGAzAAi_k7ViTAiTJDaB4Nb-voBNL-Bsg&usqp=CAU",
+    brand: "Marks & Spencer",
+    para: "Women Cateye sunglasses",
+    rs: 799,
+    price: "Rs. 799",
+    strikedoffprice: "Rs. 1199",
+    offer: "(65% OFF)",
+    wishList: "WISHLIST ♡",
+    addToBag: "ADD TO BAG",
+},
+{
+    image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSplp5UoSj5aL8GgPRLmIfOSml4oIz7xhew3w&usqp=CAU",
+    brand: "WROGN",
+    para: "Women Aviator sunglasses",
+    rs: 883,
+    price: "Rs. 883",
+    strikedoffprice: "Rs. 1699",
+    offer: "(43% OFF)",
+    wishList: "WISHLIST ♡",
+    addToBag: "ADD TO BAG",
+},
+{
+    image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRK7dHbZWbSQY_DixpCR3BEY2RYrKmal9wwlA&usqp=CAU",
+    brand: "Ted Smith",
+    para: "Women Wayfarer sunglasses",
+    rs: 594,
+    price: "Rs. 594",
+    strikedoffprice: "Rs. 1699",
+    offer: "(65% OFF)",
+    wishList: "WISHLIST ♡",
+    addToBag: "ADD TO BAG",
+},
+{
+    image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfaNXmO6xG7G-rU_AhmZm-H9o93HUix0y8wQ&usqp=CAU",
+    brand: "WROGN",
+    para: "Women Cateye sunglasses",
+    rs: 499,
+    price: "Rs. 499",
+    strikedoffprice: "Rs. 1249",
+    offer: "(65% OFF)",
+    wishList: "WISHLIST ♡",
+    addToBag: "ADD TO BAG",
+},
+{
+    image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdYFkWnZnf2MYqCK5EDlpFSygjIQHeF7XJSA&usqp=CAU",
+    brand: "Ted Smith",
+    para: "Women UV lens Sunglasses",
+    rs: 997,
+    price: "Rs. 997",
+    strikedoffprice: "Rs. 2850",
+    offer: "(65% OFF)",
+    wishList: "WISHLIST ♡",
+    addToBag: "ADD TO BAG",
+},
+{
+    image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYAKtjMR8_QyyxvhpO0rTfwfMSit2um-QX9A&usqp=CAU",
+    brand: "Marks & Spencer",
+    para: "Women Aviator sunglasses",
+    rs: 554,
+    price: "Rs. 554",
+    strikedoffprice: "Rs. 1499",
+    offer: "(65% OFF)",
+    wishList: "WISHLIST ♡",
+    addToBag: "ADD TO BAG",
+},
+];
+
+var wishList = JSON.parse(localStorage.getItem("wishListObj")) || [];
+
+var bagList = JSON.parse(localStorage.getItem("BagListObj")) || [];
+
+
+window.addEventListener('load', function(){
+    displayPage(womensData)
+  })
+
+  document.getElementById('nameSort').addEventListener('change', sortNames);
+    function sortNames(){
+      var selected = document.getElementById('nameSort').value;
+      if(selected == 'asc'){
+        womensData.sort(function(a, b){
+            if(a.brand > b.brand) return 1
+            if(a.brand < b.brand) return -1
+            return 0;
+        })
+        
+        displayPage(womensData); 
+      }
+      else{
+        womensData.sort(function(a, b){
+          if(a.brand > b.brand) return -1
+          if(a.brand < b.brand) return 1
+          return 0
+        })
+    
+        displayPage(womensData)
+      }
+    }
+
+    document.getElementById('priceSort').addEventListener('change', sortPrice); 
+    function sortPrice(){
+      var selected = document.getElementById('priceSort').value;
+      if(selected == 'lth'){
+        womensData.sort(function(a, b){
+          return a.rs-b.rs;
+        })
+        displayPage(womensData); 
+      }
+      else{
+        womensData.sort(function(a, b){
+          return b.rs - a.rs
+        })
+        displayPage(womensData)
+      }
+    }
+
+    document.getElementById('brandFilter').addEventListener('change', filterBrand);
+    function filterBrand(){
+      var selected = document.getElementById('brandFilter').value
+      var newArray = womensData.filter(function(element){
+          if(element.brand == selected){
+            return element;
+          }
+      })
+      displayPage(newArray);
+    }
+
+    function displayPage(womensData) {
+
+        document.getElementById("container").innerHTML = "";
+
+        // Total
+
+womensData.map(function(element) {
+    var box = document.createElement("div");
+    box.style.cursor = "pointer";
+    
+    var img = document.createElement("img");
+    img.src = element.image_url;
+    
+    var contentBox = document.createElement("div");
+    contentBox.setAttribute("class", "contentBox");
+    
+    var brand = document.createElement("h4");
+    brand.textContent = element.brand;
+    
+    var para = document.createElement("p");
+    para.textContent = element.para;
+    
+    var mix = document.createElement("div");
+    mix.setAttribute("class", "mixbox");
+    
+    var price = document.createElement("p");
+    price.textContent = element.price;
+    
+    var strikeprice = document.createElement("p");
+    strikeprice.textContent = element.strikedoffprice;
+    strikeprice.setAttribute("class", "strikep");
+    
+    var offer = document.createElement("p");
+    offer.textContent = element.offer;
+    offer.setAttribute("class", "offerp");
+    
+    mix.append(price, strikeprice, offer);
+    
+    var wishList = document.createElement("p");
+    wishList.setAttribute("class", "wishListp");
+    wishList.textContent = element.wishList;
+    wishList.style.cursor = "pointer";
+    
+    wishList.addEventListener("click", function() {
+        addToWishlist(element);
+        wishList.style.color = "green";
+        wishList.innerText = "ADDED TO WISHLIST";
+    });
+    
+    var addToBag = document.createElement("p");
+    addToBag.setAttribute("class", "addToBagp");
+    addToBag.textContent = element.addToBag;
+    addToBag.style.cursor = "pointer";
+    
+    addToBag.addEventListener("click", function() {
+        addToBaglist(element);
+        addToBag.innerText = "ADDED TO BAG";
+    });
+    
+    contentBox.append(brand, para, mix, wishList, addToBag);
+    box.append(img, contentBox);
+    
+    document.querySelector("#container").append(box);
+    });
+    
+    
+    // Total
+
+}
+
+
+
+function addToWishlist(element) {
+wishList.push(element);
+localStorage.setItem("wishListObj", JSON.stringify(wishList));
+}
+
+function addToBaglist(element) {
+bagList.push(element);
+localStorage.setItem("BagListObj", JSON.stringify(bagList));
+}
+
+document.getElementById("land").addEventListener("click", function() {
+window.location.href = "/LandingPage/index.html";
+});
